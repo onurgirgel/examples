23786b269467ba0595dfd9e18955ab29620aa2f0
onur <onurgirgel@gmail.com>
2025-10-12 12:16:51 +0300

    dört işlem metodu yazıldı. Ekrana çıktı verildi.

--- Metods/Program.cs
+++ Metods/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region 1. Kısım
+
+DortIslem(4, 2, global::DortIslem.Toplama);
+DortIslem(4, 2, global::DortIslem.Cikartma);
+DortIslem(4, 2, global::DortIslem.Carpma);
+DortIslem(4, 2, global::DortIslem.Bolme);
+
+static void DortIslem(double sayi1, double sayi2, DortIslem islemTipi)
+{
+    switch (islemTipi)
+    {
+        case global::DortIslem.Toplama:
+            Console.WriteLine($"Toplama işlemnin sonucu: {sayi1 + sayi2}");
+
+            break;
+        case global::DortIslem.Cikartma:
+            Console.WriteLine($"Çıkartma işlemnin sonucu: {sayi1 - sayi2}");
+
+            break;
+        case global::DortIslem.Carpma:
+            Console.WriteLine($"Çarpma işlemnin sonucu: {sayi1 * sayi2}");
+
+            break;
+        case global::DortIslem.Bolme:
+            Console.WriteLine($"Bölme işlemnin sonucu: {sayi1 / sayi2}");
+
+            break;
+        default:
+            break;
+    }
+}
+
+ enum DortIslem
+{
+    Toplama,
+    Cikartma,
+    Carpma,
+    Bolme
+}
+#endregion
