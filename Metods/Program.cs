283f6a5d667282e6e893cc17bf0895002a6dadde
onur <onurgirgel@gmail.com>
2025-10-12 12:54:23 +0300

    Geri dönüştü metod yazıldı

--- Metods/Program.cs
+++ Metods/Program.cs
@@ -1,40 +1,65 @@
 ﻿#region 1. Kısım
 
-DortIslem(4, 2, global::DortIslem.Toplama);
-DortIslem(4, 2, global::DortIslem.Cikartma);
-DortIslem(4, 2, global::DortIslem.Carpma);
-DortIslem(4, 2, global::DortIslem.Bolme);
+//DortIslemParametreli(4, 2, global::DortIslem.Toplama);
+//DortIslemParametreli(4, 2, global::DortIslem.Cikartma);
+//DortIslemParametreli(4, 2, global::DortIslem.Carpma);
+//DortIslemParametreli(4, 2, global::DortIslem.Bolme);
 
-static void DortIslem(double sayi1, double sayi2, DortIslem islemTipi)
+//static void DortIslemParametreli(double sayi1, double sayi2, DortIslem islemTipi)
+//{
+//    switch (islemTipi)
+//    {
+//        case global::DortIslem.Toplama:
+//            Console.WriteLine($"Toplama işlemnin sonucu: {sayi1 + sayi2}");
+
+//            break;
+//        case global::DortIslem.Cikartma:
+//            Console.WriteLine($"Çıkartma işlemnin sonucu: {sayi1 - sayi2}");
+
+//            break;
+//        case global::DortIslem.Carpma:
+//            Console.WriteLine($"Çarpma işlemnin sonucu: {sayi1 * sayi2}");
+
+//            break;
+//        case global::DortIslem.Bolme:
+//            Console.WriteLine($"Bölme işlemnin sonucu: {sayi1 / sayi2}");
+
+//            break;
+//        default:
+//            break;
+//    }
+//}
+
+
+#endregion
+
+#region 2. Kısım 
+static double DortIslemGeriDonuslu(int sayi1, int sayi2, DortIslem islemTipi)
 {
     switch (islemTipi)
     {
-        case global::DortIslem.Toplama:
-            Console.WriteLine($"Toplama işlemnin sonucu: {sayi1 + sayi2}");
-
-            break;
-        case global::DortIslem.Cikartma:
-            Console.WriteLine($"Çıkartma işlemnin sonucu: {sayi1 - sayi2}");
-
-            break;
-        case global::DortIslem.Carpma:
-            Console.WriteLine($"Çarpma işlemnin sonucu: {sayi1 * sayi2}");
-
-            break;
-        case global::DortIslem.Bolme:
-            Console.WriteLine($"Bölme işlemnin sonucu: {sayi1 / sayi2}");
-
-            break;
+        case DortIslem.Toplama:
+            return sayi1 + sayi2;
+        case DortIslem.Cikartma:
+            return sayi1 - sayi2;
+        case DortIslem.Carpma:
+            return sayi1 * sayi2;
+        case DortIslem.Bolme:
+            return sayi1 / sayi2;
         default:
-            break;
+            return 0;
     }
 }
+double sonuc = DortIslemGeriDonuslu(4, 2, DortIslem.Toplama);
+Console.WriteLine($"Toplama işleminin sonucu: {sonuc}");
+#endregion
 
- enum DortIslem
+
+
+enum DortIslem
 {
     Toplama,
     Cikartma,
     Carpma,
     Bolme
 }
-#endregion
