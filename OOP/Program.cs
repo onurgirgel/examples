89fd668a26437f43d3cded95aaccce461a78c1b2
onur <onurgirgel@gmail.com>
2025-10-18 12:11:10 +0300

    Polymorphism örneği yapıldı

---
+++ OOP/Program.cs
@@ -0,0 +1,19 @@
+﻿#region Constructor / Desctructor
+// ÖDEV
+#endregion
+
+#region Encapsulation (Kapsülleme)
+// ÖDEV
+#endregion
+
+#region Inheritance (Kalıtım)
+// ÖDEV
+#endregion
+
+#region Polymorphism (Çok Biçimlilik)
+using OOP.Polymorphism;
+
+Dog dog = new Dog();
+dog.Sound();
+
+#endregion
