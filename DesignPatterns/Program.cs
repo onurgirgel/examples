a1a1ee0dcbd87553d44089014fb629bfa0c32672
onur <onurgirgel@gmail.com>
2025-11-15 12:47:47 +0300

    Strategy Pattern tasarımı yapıldı

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,4 +1,6 @@
-﻿//using DesignPatterns.Singleton;
+﻿
+#region SINGLETON PATTERN
+//using DesignPatterns.Singleton;
 //using DesignPatterns.Student;
 
 //var logger = Logger.GetInstance();
@@ -6,11 +8,43 @@
 
 //StudentService studentService = new StudentService(logger);
 //studentService.Add();
+#endregion
 
-using DesignPatterns.Factory;
-using DesignPatterns.Singleton;
+#region FACTORY PATTERN
+//using DesignPatterns.Factory;
+//using DesignPatterns.Singleton;
 
-ISekil sekilDaire = SekilFactory.GetSekil("daire");
-ISekil sekilKare = SekilFactory.GetSekil("kare");
-sekilDaire.Ciz();
-sekilKare.Ciz();
+//ISekil sekilDaire = SekilFactory.GetSekil("daire");
+//ISekil sekilKare = SekilFactory.GetSekil("kare");
+//sekilDaire.Ciz();
+//sekilKare.Ciz();
+#endregion
+
+#region STRATEGY PATTERN
+using DesignPatterns.Strategy;
+
+var cebimdekipara = 1000;
+double anlikfiyat = 0;
+Console.Write("Lütfen ürün kodunu söyle:");
+string kod = Console.ReadLine();
+if (kod == "adidas")
+    anlikfiyat = 1100;
+else if (kod == "zara")
+    anlikfiyat = 1300;
+else
+    anlikfiyat = 1000;
+
+if (anlikfiyat > cebimdekipara)
+{
+    //KREDİ KARTI
+    var f = new FiyatStrategy { strateji = new Yuzde10() };
+    Console.WriteLine($"Ödeme yöntemi kredi kartı --> ürün satış fiyati {anlikfiyat}, indirimli fiyatı {f.Hesapla(anlikfiyat)} ");
+}
+else
+{
+    // NAKİT
+    var f = new FiyatStrategy { strateji = new Yuzde20() };
+    Console.WriteLine($"Ödeme yöntemi nakit --> ürün satış fiyati {anlikfiyat}, indirimli fiyatı {f.Hesapla(anlikfiyat)} ");
+}
+
+#endregion
---
+++ DesignPatterns/Strategy/Indirim.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Strategy
+{
+
+    public class Yuzde10 : IIndirim
+    {
+        public double Hesapla(double tutar) => tutar * 0.9;
+    }
+    public class Yuzde20 : IIndirim
+    {
+        public double Hesapla(double tutar) { return tutar * 0.8; }
+    }
+}
